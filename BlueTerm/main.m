8b8b921ca8100a086adc73c86d868cb2ff302cc1
Thomas Buck <xythobuz@xythobuz.de>
2017-05-29 23:28:54 +0200

    Initial commit

---
+++ BlueTerm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BlueTerm
+//
+//  Created by Thomas Buck on 29.05.17.
+//  Copyright © 2017 Thomas Buck. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
